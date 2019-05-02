059c350566c2f2df0bd503b70601c60849371e48
Mikael H Subianto <mikaelhsubianto@airbears2-10-142-9-150.airbears2.1918.berkeley.edu>
2019-05-02 00:55:57 -0700

    susah

--- datacollector.py
+++ datacollector.py
@@ -13,59 +13,61 @@ def solve(client):
     # f = open("results.txt", "a+")
 
     client.end()
-    client.start()
-
-    graph = client.G
-
-    locations = list(graph.nodes())
-    numlocations = len(list(graph.nodes()))
-    print("numlocations is ", str(numlocations))
-
-    all_students = list(range(1, client.students + 1))
-    numstudents = client.students
-
-    print("num students is", str(numstudents))
-
-    minNumTruth = ceil(numlocations / 2)
-
-    numTruth = [0 for _ in range(numstudents)]
-    numLies = [0 for _ in range(numstudents)]
 
     matrix = []
 
-    #evaluate all locations but in random order
-    loc = list(range(numlocations))
-    random.shuffle(loc)
+    for i in range(90):
+        client.start()
 
-    for randomloc in loc:
-        #scount on location and collect student reports
-        temp_locations = client.scout(locations[randomloc], all_students)
-        if temp_locations is None:
-            continue
-        studentreports = list(temp_locations.values())
+        graph = client.G
 
-        #remote on location get the actual value
-        target = locations[randomloc]
-        nextEdges = list(graph.edges(target, 'weight', default=0))
-        minEdgeIndex = 0
-        minEdge = -1
-        for i in range(len(nextEdges)):
-            if i == 0:
-                minEdge = nextEdges[i][2]
-                minEdgeIndex = i
-            if nextEdges[i][2] < minEdge:
-                minEdge = nextEdges[i][2]
-                minEdgeIndex = i
-        actualvalue = int(client.remote(target, nextEdges[minEdgeIndex][1]))
+        locations = list(graph.nodes())
+        numlocations = len(list(graph.nodes()))
+        print("numlocations is ", str(numlocations))
 
-        for i in range(numstudents):
-            studentreport = int(studentreports[i])
-            if studentreport == actualvalue: #is telling the truth
-                numTruth[i] += 1
-            else:
-                numLies[i] -=1
-            worstcaseprob = (minNumTruth - numTruth[i]) / (numlocations - numTruth[i] - numLies[i])
-            matrix.append([worstcaseprob, studentreport, actualvalue])
+        all_students = list(range(1, client.students + 1))
+        numstudents = client.students
+
+        print("num students is", str(numstudents))
+
+        minNumTruth = ceil(numlocations / 2)
+
+        numTruth = [0 for _ in range(numstudents)]
+        numLies = [0 for _ in range(numstudents)]
+
+        #evaluate all locations but in random order
+        loc = list(range(numlocations))
+        random.shuffle(loc)
+
+        for randomloc in loc:
+            #scount on location and collect student reports
+            temp_locations = client.scout(locations[randomloc], all_students)
+            if temp_locations is None:
+                continue
+            studentreports = list(temp_locations.values())
+
+            #remote on location get the actual value
+            target = locations[randomloc]
+            nextEdges = list(graph.edges(target, 'weight', default=0))
+            minEdgeIndex = 0
+            minEdge = -1
+            for i in range(len(nextEdges)):
+                if i == 0:
+                    minEdge = nextEdges[i][2]
+                    minEdgeIndex = i
+                if nextEdges[i][2] < minEdge:
+                    minEdge = nextEdges[i][2]
+                    minEdgeIndex = i
+            actualvalue = int(client.remote(target, nextEdges[minEdgeIndex][1]))
+
+            for i in range(numstudents):
+                studentreport = int(studentreports[i])
+                if studentreport == actualvalue: #is telling the truth
+                    numTruth[i] += 1
+                else:
+                    numLies[i] -=1
+                worstcaseprob = (minNumTruth - numTruth[i]) / (numlocations - numTruth[i] - numLies[i])
+                matrix.append([worstcaseprob, studentreport, actualvalue])
 
     np.savetxt("results.txt", np.array(matrix, np.float64))
     # f.write(str(matrix))
