76dfaaf9581f3fa476b5d54aca22bb08d44d796b
Brian Unggul <brianunggul@airbears2-10-142-16-31.airbears2.1918.berkeley.edu>
2019-05-02 00:50:36 -0700

    fixed the datacollector.py and scikitml.py

--- scikitml.py
+++ scikitml.py
@@ -1,17 +1,19 @@
-import pandas, pickle, os
+import pandas as pd
+import pickle, os
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 
 url = ""
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 basePath = os.path.dirname(os.path.abspath(__file__))
-path = basePath + "/test_graphs/singapore_0.read_json"
-dataframe = pandas.read_json(path)
+path = basePath + '/results.txt'
+df = pd.read_csv(path, sep=" ", header=None)
+df.columns = ['worstcaseprob', 'scout_report', 'actualvalue']
 
-matrix = dataframe.values
-X = matrix[:,0:8]
-Y = matrix[:,8]
+data = df.values
+# print(data)
 test_size = 0.3
+X, Y = data[:, :1], data[:, 2]
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size)
 
 # Fit the model on 33%
@@ -27,4 +29,4 @@ pickle.dump(model, open(filename, 'wb'))
 # load the model from disk
 loaded_model = pickle.load(open(filename, 'rb'))
 result = loaded_model.score(X_test, Y_test)
-display(result)
+print(result)
